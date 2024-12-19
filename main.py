b266bf11313df1201fbe95be16925a3f04b20e4e
Vionaaru <d.s.kasimov@gmail.com>
2024-12-19 22:01:42 +0300

    add poetry

--- main.py
+++ main.py
@@ -8,21 +8,21 @@ API_TOKEN = '7469198396:AAGfhnb-A8l-suGAf23mxRnIECNoxwQwfxM'
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
-# Функция для создания обычной клавиатуры с webapp кнопками
+# Функция для создания обычной клавиатуры с WebApp кнопками
 def web_app_keyboard():
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
     web_app_test = WebAppInfo(url="https://weather.nsu.ru")
-    web_app_game = WebAppInfo(url="https://llmaccess3.grabsdigital.com")
+    web_app_game = WebAppInfo(url="https://vionaaru.github.io/webapp001")
     one_button = KeyboardButton(text="Тестовая страница", web_app=web_app_test)
-    two_button = KeyboardButton(text="Эбигейл", web_app=web_app_game)
+    two_button = KeyboardButton(text="Запустить WebApp", web_app=web_app_game)
     keyboard.add(one_button, two_button)
     return keyboard
 
-# Функция для создания inline-клавиатуры с webapp кнопками
+# Функция для создания inline-клавиатуры с WebApp кнопками
 def web_app_keyboard_inline():
     keyboard = InlineKeyboardMarkup(row_width=1)
-    web_app = WebAppInfo(url="https://llmaccess3.grabsdigital.com")
-    inline_button = InlineKeyboardButton(text="Веб Эбигейл", web_app=web_app)
+    web_app = WebAppInfo(url="https://vionaaru.github.io/webapp001/")
+    inline_button = InlineKeyboardButton(text="Запустить Inline WebApp", web_app=web_app)
     keyboard.add(inline_button)
     return keyboard
 
@@ -30,8 +30,7 @@ def web_app_keyboard_inline():
 @dp.message(Command("start"))
 async def start_fun(message: Message):
     await message.answer(
-        "Привет, я бот для проверки Telegram WebApps! 🖥\n"
-        "Страницы можно запустить, нажав на кнопки.",
+        "Привет! Нажми кнопку ниже, чтобы открыть WebApp:",
         reply_markup=web_app_keyboard()
     )
 
@@ -48,13 +47,13 @@ async def inline_fun(message: Message):
 async def new_mes(message: Message):
     await start_fun(message)
 
-# Обработчик данных, полученных из web-app
+# Обработчик данных, полученных из WebApp
 @dp.message(content_types={"web_app_data"})
 async def answer(web_app_mes: Message):
     print(web_app_mes)  # Вся информация о сообщении
     print(web_app_mes.web_app_data.data)  # Конкретные данные, отправленные из WebApp
     await web_app_mes.answer(
-        f"Получили информацию из веб-приложения: {web_app_mes.web_app_data.data}"
+        f"Получены данные из WebApp: {web_app_mes.web_app_data.data}"
     )
 
 # Основная функция
