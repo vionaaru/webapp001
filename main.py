30358e3cc8713fd695a79385140091a9dac4dd2d
Vionaaru <d.s.kasimov@gmail.com>
2024-12-20 00:52:57 +0300

    index

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, WebAppInfo, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 from aiogram.filters import Command
 from aiogram.types.message import ContentType
 
@@ -9,23 +9,12 @@ API_TOKEN = '7469198396:AAGfhnb-A8l-suGAf23mxRnIECNoxwQwfxM'
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
-# Функция для создания обычной клавиатуры с WebApp кнопками
-def web_app_keyboard():
-    web_app_game = WebAppInfo(url="https://vionaaru.github.io/webapp001")
-    keyboard = ReplyKeyboardMarkup(
-        keyboard=[
-            [KeyboardButton(text="Запустить WebApp", web_app=web_app_game)],
-        ],
-        resize_keyboard=True
-    )
-    return keyboard
-
 # Функция для создания inline-клавиатуры с WebApp кнопками
 def web_app_keyboard_inline():
-    web_app = WebAppInfo(url="https://vionaaru.github.io/webapp001/")
+    web_app = WebAppInfo(url="https://vionaaru.github.io/webapp001/")  # URL вашего WebApp
     keyboard = InlineKeyboardMarkup(
         inline_keyboard=[
-            [InlineKeyboardButton(text="Запустить Inline WebApp", web_app=web_app)]
+            [InlineKeyboardButton(text="Запустить WebApp", web_app=web_app)]
         ]
     )
     return keyboard
@@ -38,19 +27,11 @@ async def start_fun(message: Message):
         reply_markup=web_app_keyboard_inline()
     )
 
-# Обработчик команды /inline
-@dp.message(Command("inline"))
-async def inline_fun(message: Message):
-    await message.answer(
-        "Вот inline-клавиатура для запуска WebApp:",
-        reply_markup=web_app_keyboard()
-    )
-
 # Обработчик данных, полученных из WebApp
 @dp.message(lambda msg: msg.content_type == ContentType.WEB_APP_DATA)
 async def answer(web_app_mes: Message):
-    print(web_app_mes)  # Вся информация о сообщении
-    print(web_app_mes.web_app_data.data)  # Конкретные данные, отправленные из WebApp
+    print("Получено сообщение от WebApp:", web_app_mes)
+    print("Данные:", web_app_mes.web_app_data.data)
     await web_app_mes.answer(
         f"Получены данные из WebApp: {web_app_mes.web_app_data.data}"
     )
