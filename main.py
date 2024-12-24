2b7e37af163eae157bf825be336858d6d87c4794
Vionaaru <d.s.kasimov@gmail.com>
2024-12-24 18:17:33 +0300

    add query_id

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo, InlineQueryResultArticle, InputTextMessageContent, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo, ReplyKeyboardMarkup, \
+    KeyboardButton
 from aiogram.filters import Command
 from aiogram.types.message import ContentType
-
 from environs import Env
+
 # Загружаем переменные окружения
 env = Env()
 env.read_env()
 
-TG_TOKEN = env.str("TG_TOKEN")
+API_TOKEN = env.str("TG_TOKEN")
 
-bot = Bot(token=TG_TOKEN)
+bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
+
 
 # Функция для создания inline-клавиатуры с WebApp кнопками
 def web_app_keyboard_inline():
@@ -24,6 +26,7 @@ def web_app_keyboard_inline():
     )
     return keyboard
 
+
 # Функция для создания обычной клавиатуры с WebApp кнопкой
 def web_app_keyboard():
     web_app = WebAppInfo(url="https://vionaaru.github.io/webapp001/")
@@ -34,6 +37,7 @@ def web_app_keyboard():
         resize_keyboard=True
     )
     return keyboard
+
 
 # Обработчик команды /start
 @dp.message(Command("start"))
@@ -47,16 +51,25 @@ async def start_fun(message: Message):
         reply_markup=web_app_keyboard()
     )
 
+
 # Обработчик данных, полученных из WebApp
 @dp.message(lambda msg: msg.content_type == ContentType.WEB_APP_DATA)
 async def handle_web_app_data(message: Message):
     web_app_data = message.web_app_data.data
-    print(f"Получены данные: {web_app_data}")
+    query_id = getattr(message.web_app_data, "query_id", None)
+
+    if query_id:
+        print(f"Получены данные с query_id: {web_app_data}")
+    else:
+        print(f"Получены данные без query_id: {web_app_data}")
+
     await message.answer(f"Получены данные из WebApp: {web_app_data}")
+
 
 # Основная функция
 async def main():
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
